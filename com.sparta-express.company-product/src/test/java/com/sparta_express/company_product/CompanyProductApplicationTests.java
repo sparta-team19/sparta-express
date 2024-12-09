0c6df51bddc8e42c875f77179f8e76da67890701
cylcoder <cylcoder@gmail.com>
2024-12-09 11:52:54 +0900

    setup: company-product 및 order-shipment 서비스 초기화 및 설정

---
+++ com.sparta-express.company-product/src/test/java/com/sparta_express/company_product/CompanyProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta_express.company_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
