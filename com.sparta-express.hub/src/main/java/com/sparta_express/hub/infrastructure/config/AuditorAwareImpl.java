278866695860224c3b14c085c45017f25336bf59
김성용 <tjddyd1565@naver.com>
2024-12-13 14:59:07 +0900

    feat #13: jpaAuditing user로그인 id 저장토록 수정

--- com.sparta-express.hub/src/main/java/com/sparta_express/hub/infrastructure/config/AuditorAwareImpl.java
+++ com.sparta-express.hub/src/main/java/com/sparta_express/hub/infrastructure/config/AuditorAwareImpl.java
@@ -1,7 +1,5 @@
 package com.sparta_express.hub.infrastructure.config;
 
-import com.sparta_express.hub.common.exception.CustomException;
-import com.sparta_express.hub.common.exception.ErrorType;
 import com.sparta_express.hub.common.utils.RequestExtractor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
@@ -9,23 +7,13 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component
-public class AuditorAwareImpl implements AuditorAware<Long> {
-
+public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
-    public Optional<Long> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
 
-        String userIdHeader
+        String userId
                 = RequestExtractor.extractHeaderOf("X-User-Id");
-
-        long userId;//todo 멘토님 문의후 id를 pk , loginId중 결정
-        try {
-            userId = Long.parseLong(userIdHeader);
-
-        } catch (NumberFormatException e) {
-            throw new CustomException(ErrorType.USER_ID_INVALID);
-
-        }
 
         return Optional.of(userId);
     }
