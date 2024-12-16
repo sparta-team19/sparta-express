c26fef4c9657e8a0df0e1ede9839f2aea6eb309d
Woojin Kim <rladnwlsok@naver.com>
2024-12-16 09:34:02 +0900

    fix #7: findMaxDeliverySequence의 자료형이 DeliverySequence와 달라서 JPQL 쿼리 사용

--- com.sparta-express.auth/src/main/java/com/sparta_express/auth/user/repository/DeliveryManagerRepository.java
+++ com.sparta-express.auth/src/main/java/com/sparta_express/auth/user/repository/DeliveryManagerRepository.java
@@ -2,12 +2,12 @@ package com.sparta_express.auth.user.repository;
 
 import com.querydsl.core.types.Predicate;
 import com.sparta_express.auth.user.entity.DeliveryManager;
-import com.sparta_express.auth.user.entity.User;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface DeliveryManagerRepository extends JpaRepository<DeliveryManager, UUID>,
@@ -17,6 +17,7 @@ public interface DeliveryManagerRepository extends JpaRepository<DeliveryManager
 
     Optional<DeliveryManager> findByUserId(Long id);
 
+    @Query("SELECT MAX(d.deliverySequence) FROM DeliveryManager d")
     Integer findMaxDeliverySequence();
 
     Page<DeliveryManager> findAll(Predicate predicate, Pageable pageable);
