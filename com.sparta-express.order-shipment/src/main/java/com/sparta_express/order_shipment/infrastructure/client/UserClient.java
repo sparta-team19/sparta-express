ca55bd661a5e3d4e64c95de0f731bcba72f9aac8
sohyuneeee <sohyuney@naver.com>
2024-12-17 11:52:17 +0900

    refactor: feignclient로 유저 조회 수정 email 로 조회

--- com.sparta-express.order-shipment/src/main/java/com/sparta_express/order_shipment/infrastructure/client/UserClient.java
+++ com.sparta-express.order-shipment/src/main/java/com/sparta_express/order_shipment/infrastructure/client/UserClient.java
@@ -3,6 +3,7 @@ package com.sparta_express.order_shipment.infrastructure.client;
 import com.sparta_express.order_shipment.application.dto.ResponseDataDto;
 import com.sparta_express.order_shipment.config.FeignClientConfig;
 import com.sparta_express.order_shipment.infrastructure.client.fallback.UserFallbackFactory;
+import com.sparta_express.order_shipment.infrastructure.dto.DeliveryManagerResponseDto;
 import com.sparta_express.order_shipment.infrastructure.dto.UserResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface UserClient {
 
     @GetMapping("/api/v1/users/{email}")
-    ResponseDataDto<UserResponseDto> getUser(@PathVariable String email);
+    ResponseDataDto<UserResponseDto> getUserByEmail(@PathVariable String email);
 
     @GetMapping("/api/v1/users/delivery/search")
-    ResponseDataDto<UserResponseDto> getDeliveryManager();
+    ResponseDataDto<DeliveryManagerResponseDto> getDeliveryManager();
 
 }
