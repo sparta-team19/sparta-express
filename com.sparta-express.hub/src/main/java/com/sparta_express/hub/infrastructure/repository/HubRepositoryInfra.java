ea792adfe1cf46d3f3641bd35c2f5650e7abca0c
김성용 <tjddyd1565@naver.com>
2024-12-18 23:24:45 +0900

    refac #13: 테스트 케이스 db 저장/읽기용 메서드 삭제, TestController도 맞춰서 수정

--- com.sparta-express.hub/src/main/java/com/sparta_express/hub/infrastructure/repository/HubRepositoryInfra.java
+++ com.sparta-express.hub/src/main/java/com/sparta_express/hub/infrastructure/repository/HubRepositoryInfra.java
@@ -3,7 +3,7 @@ package com.sparta_express.hub.infrastructure.repository;
 import com.sparta_express.hub.common.exception.CustomException;
 import com.sparta_express.hub.common.exception.ErrorType;
 import com.sparta_express.hub.domain.HubRepository;
-import com.sparta_express.hub.domain.model.Hub;
+import com.sparta_express.hub.Hub;
 import com.sparta_express.hub.domain.model.InterhubRoute;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -30,17 +30,6 @@ public class HubRepositoryInfra implements HubRepository {
     @Override
     public List<InterhubRoute> readAllInterhubRoutes() {
         return interhubRouteJpaRepo.findAll();
-    }
-
-
-    @Override
-    public void saveAllHubs(List<Hub> hubs) {
-        hubJpaRepo.saveAll(hubs);
-    }
-
-    @Override
-    public void saveAllInterhubRoutes(List<InterhubRoute> interhubRoutes) {
-        interhubRouteJpaRepo.saveAll(interhubRoutes);
     }
 
     public InterhubRoute readInterhubRoute(UUID interhubRouteId) {
